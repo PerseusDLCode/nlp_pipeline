c211af593e97b62575d702d54af5b40dbd96ef09
Charles Pletcher <cpletcher2@gmail.com>
2026-05-07 17:13:21 -0400

    Attempt to generate API documentation on push

---
+++ scripts/generate_openapi_json.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+import json
+
+from fastapi.openapi.utils import get_openapi
+from nlp_pipeline.server import app
+
+
+with open("openapi.json", "w") as f:
+    json.dump(
+        get_openapi(
+            title=app.title,
+            version=app.version,
+            openapi_version=app.openapi_version,
+            description=app.description,
+            routes=app.routes,
+        ),
+        f,
+    )
